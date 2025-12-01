10fc975b471eaa78bfef9558beb246e82a4c858c
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2025-12-01 07:14:05 -0800

    test

---
+++ Robust.Shared/Toolshed/TypeParsers/TimeSpanTypeParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Robust.Shared.Console;
+using Robust.Shared.Toolshed.Syntax;
+
+namespace Robust.Shared.Toolshed.TypeParsers;
+
+public sealed class TimeSpanTypeParser : TypeParser<TimeSpan>
+{
+    public override bool TryParse(ParserContext ctx, out TimeSpan result)
+    {
+        result = TimeSpan.Zero;
+        var span = ctx.GetWord(ParserContext.IsToken);
+
+        return span is not null && TimeSpan.TryParse(span, out result);
+    }
+
+    public override CompletionResult? TryAutocomplete(ParserContext ctx, CommandArgument? arg)
+    {
+        return CompletionResult.FromHint(GetArgHint(arg));
+    }
+}
